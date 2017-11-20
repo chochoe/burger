e07415c0663a6da8792aadfb0c4c3b818183143f
Emery Cho <cho.e@live.com>
2017-11-19 21:53:13 -0800

    progressing on setup

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,22 @@
 // selectAll();
 // insertOne();
 // updateOne();
+var db = require("./connection");
+
+var orm = {
+	selectAll: function(cb) {
+		db.query("SELECT * FROM burgers", function(err, res) {
+			if (err) {
+				throw err;
+			}
+			cb(res);
+		})
+	},
+	insertOne: function() {},
+	devour: function() {}
+}
+
+
+orm.selectAll(console.log);
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var handlebars = require("express-handlebars");
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {}
+
+
+/* 
+step 1: page loads + get current burgers from db
+step 2: render burgers in appropriate places
+
+step 3: when user adds burger, submit form to route /api/burger
+step 4: server inserts burger into db
+
+step 5: repeat steps 1 and 2
+
+step 6: when user devours, update target to "devoured" list
+step 7: repeat steps 1 and 2 (function for this makes it better)
+*/
